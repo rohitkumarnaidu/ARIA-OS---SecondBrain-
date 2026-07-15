d697fb61a989d152a187fd3b7784a5d263534a99
Bappadala Rohith Kumar Naidu <198095685+rohitkumarnaidu@users.noreply.github.com>
2026-07-15 17:36:38 +0530

    chore: Update miscellaneous files (part 27)

--- apps/web/lib/offline/index.ts
+++ apps/web/lib/offline/index.ts
@@ -1,3 +1,8 @@
+export { offlineDb } from './db'
+export type { StoreName, StoredRecord, SyncQueueItem } from './db'
+export { syncManager } from './sync'
+export { useOfflineStore } from './store'
+export type { OfflineState } from './store'
+
 export { offlineQueue } from './queue'
 export { offlineStorage } from './storage'
-export { syncManager } from './sync'
